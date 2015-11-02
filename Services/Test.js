f6c4cdc0c9fe22e5be8ccc956c16b0cbde68efe6
Marius Hartig <marius.hartig@googlemail.com>
2015-11-02 21:25:10 +0100

    bug fixing

--- Services/Test.js
+++ Services/Test.js
@@ -2,7 +2,11 @@ function Test() {
 
 }
 Test.prototype.showMessage = function(p1,p2) {
-  return this.message + " " + p1 + " " + p2;
+  if(this.parameter) {
+      return this.parameter.message + " " + p1 + " " + p2;
+  } else {
+    return "Hallo";
+  }
 }
 
 exports.object = Test;
--- config.js
+++ config.js
@@ -1,19 +1,25 @@
 
 var config = {
+  
   port : 8080,
   host: '127.0.0.1',
   contentType: "text/plain",
   errorContentType: "text/plain",
-  objectRules:[
-  "filemanger"
-  ],
-  documentRules: [
-  ""
-  ],
+
   extensionRules: {
+
     txt: {
       contentType : "text/plain"
+    },
+
+    html: {
+      contentType : "text/html"
+    },
+
+    css: {
+      contentType: "text/css"
     }
+
   }
 }
 
