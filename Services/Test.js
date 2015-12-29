b7bff1b39f95fa88a39759d3cf856bbcb5802b77
Marius Hartig <marius.hartig@googlemail.com>
2015-12-29 22:49:48 +0100

    implemented cookie function

--- Services/Test.js
+++ Services/Test.js
@@ -1,7 +1,13 @@
-
+let __cookie = require(__dirname + "/../lib/cookies/Cookie.js");
 
 class Test {
   showMessage(p1,p2) {
+    console.log(this.provider.cookieManager.cookies);
+    var cookie = new __cookie.class("cookie");
+    cookie.addValue("key1","value1");
+    cookie.addValue("key2","value2");
+    this.provider.cookieManager.addCookie(cookie);
+
     if(this.parameter) {
         return "hdhdhdhd :D hammer " + this.parameter.message + " " + p1 + " " + p2;
     } else {
